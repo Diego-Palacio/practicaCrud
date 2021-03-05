2d37dede7d4e7355c493efd431dd4e168bef4a95
Diego <diego_palacio2015@hotmail.com>
2021-03-04 22:23:54 -0300

    habilidades pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -5,17 +5,18 @@ const Pokemon = () =>{
 
 const [pokemons,setPokemon]= useState("");
 const [imagen,setImagen]= useState();
+const [habilidades,setHabilidades]=useState([]);
 
 const obtenerPokemon=async()=> {
     const apii = await axios .get('https://pokeapi.co/api/v2/pokemon/24');
     const usarApi= await apii.data;
-    console.log(apii);
-
-
+ 
     setPokemon(usarApi.name);
    
     setImagen(usarApi.sprites.back_default);
-    console.log(usarApi.sprites.back_default);
+    console.log(apii)
+    console.log(usarApi.abilities);
+    setHabilidades(usarApi.abilities);
 }
 
     
@@ -25,9 +26,23 @@ const obtenerPokemon=async()=> {
 
 
 return(
-        <div>
+        <div className="contenedorPokemon">
                 <h1 className="textoPokemon"> {pokemons} </h1>
-                <img src={imagen} class="imagen"/>
+                <img src={imagen} className="imagen"/>
+                <h2 className="habilidades"> {} </h2>
+
+              {
+                  habilidades.map(item =>
+
+                     <div>
+                            <p key= {item.slot} > {item.ability.name} </p>
+                     </div>
+                  )
+
+              }
+
+                
+
         </div>
 
 
